424562e2c4dd7aa7644ced1f358fd7ac4ba9506f
kothru <1894683+kothru@users.noreply.github.com>
2022-07-05 22:17:57 +0900

    asserteq

--- src/lib.rs
+++ src/lib.rs
@@ -28,6 +28,17 @@ mod tests {
         };
         assert!(larger.can_hold(&smaller));
     }
+
+    #[test]
+    fn it_adds_two() {
+        // Rust test is no order expected, actual
+        assert_eq!(4, add_two(2));
+        assert_ne!(3, add_two(2));
+    }
+}
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
 }
 
 #[derive(Debug)]
